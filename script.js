225302df1698586f2f90c739a382d2da73d31c22
black-RAM <pplanes311@gmail.com>
2023-05-09 13:01:17 +0300

    for loop produces a grid, which can be any number of rows

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let grid = document.getElementById('container');
 
 // create one row
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < 16*6; i++) {
   let gridElement = document.createElement('div');
   gridElement.classList.add('grid-element');
   grid.appendChild(gridElement)
