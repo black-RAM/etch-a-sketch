babd2496d424098481f27a9587efb828eb223fb3
black-RAM <pplanes311@gmail.com>
2023-05-11 18:07:59 +0300

    createGrid() function
    For the user to choose their aspect ratio and resolution (a feature to be added later) create grid is now a function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let grid = document.getElementById('container');
+/* grid creation is in function to more easily control parameters. Parameters:
+res = resolution = number of columns. Named as such because
+higher column number = smaller "pixels" = higher "resolution".
+ratio = aspect ratio; 1:1 input as [1, 1] */
 
-// create one row
-for(let i = 0; i < 16*6; i++) {
-  let gridElement = document.createElement('div');
-  gridElement.classList.add('grid-element');
-  grid.appendChild(gridElement)
+function createGrid(res, ratio) {
+  const [w, h] = ratio;
+  let grid = document.getElementById('grid-container');
+  grid.style.gridTemplateColumns = `repeat(${res}, auto)`;
+
+  for (let i = 0; i < res * 6; i++) {
+    let gridElement = document.createElement('div');
+    gridElement.classList.add('grid-element');
+    grid.appendChild(gridElement);
+  }
 }
+
+createGrid(10, [1, 1]);
